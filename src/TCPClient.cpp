d907b0ca9cb0f7c116b27a6bc332eb131f203614
yangjian.yangjian <yangjian.yangjian@alibaba-inc.com>
2018-07-29 17:42:57 +0800

    fix

--- src/TCPClient.cpp
+++ src/TCPClient.cpp
@@ -7,52 +7,52 @@ TCPClient::TCPClient()
 	address = "";
 }
 
-bool TCPClient::setup(string address , int port)
+bool TCPClient::setup(string address, int port)
 {
-  	if(sock == -1)
+	if (sock == -1)
 	{
-		sock = socket(AF_INET , SOCK_STREAM , 0);
+		sock = socket(AF_INET, SOCK_STREAM, 0);
 		if (sock == -1)
 		{
-      			cout << "Could not create socket" << endl;
-    		}
-        }
-  	if(inet_addr(address.c_str()) == -1)
-  	{
-    		struct hostent *he;
-    		struct in_addr **addr_list;
-    		if ( (he = gethostbyname( address.c_str() ) ) == NULL)
-    		{
-		      herror("gethostbyname");
-      		      cout<<"Failed to resolve hostname\n";
-		      return false;
-    		}
-	   	addr_list = (struct in_addr **) he->h_addr_list;
-    		for(int i = 0; addr_list[i] != NULL; i++)
-    		{
-      		      server.sin_addr = *addr_list[i];
-		      break;
-    		}
-  	}
-  	else
-  	{
-    		server.sin_addr.s_addr = inet_addr( address.c_str() );
-  	}
-  	server.sin_family = AF_INET;
-  	server.sin_port = htons( port );
-  	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-  	{
-    		perror("connect failed. Error");
-    		return false;
-  	}
-  	return true;
+			cout << "Could not create socket" << endl;
+		}
+	}
+	if (inet_addr(address.c_str()) == -1)
+	{
+		struct hostent *he;
+		struct in_addr **addr_list;
+		if ((he = gethostbyname(address.c_str())) == NULL)
+		{
+			herror("gethostbyname");
+			cout << "Failed to resolve hostname\n";
+			return false;
+		}
+		addr_list = (struct in_addr **)he->h_addr_list;
+		for (int i = 0; addr_list[i] != NULL; i++)
+		{
+			server.sin_addr = *addr_list[i];
+			break;
+		}
+	}
+	else
+	{
+		server.sin_addr.s_addr = inet_addr(address.c_str());
+	}
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+	{
+		perror("connect failed. Error");
+		return false;
+	}
+	return true;
 }
 
 bool TCPClient::Send(string data)
 {
-	if(sock != -1)
+	if (sock != -1)
 	{
-		if( send(sock , data.c_str() , strlen( data.c_str() ) , 0) < 0)
+		if (send(sock, data.c_str(), strlen(data.c_str()), 0) < 0)
 		{
 			cout << "Send failed : " << data << endl;
 			return false;
@@ -65,27 +65,24 @@ bool TCPClient::Send(string data)
 
 string TCPClient::receive(int size)
 {
-  	char buffer[size];
+	char buffer[size];
 	memset(&buffer[0], 0, sizeof(buffer));
 
-  	string reply;
-	if( recv(sock , buffer , size, 0) < 0)
-  	{
-	    	cout << "receive failed!" << endl;
+	string reply;
+	if (recv(sock, buffer, size, 0) < 0)
+	{
+		cout << "receive failed!" << endl;
 		return nullptr;
-  	}
-  	cout<<"buffer length=="<<strlen(buffer)<<endl;
-	buffer[size-1]='\0';
-	for(int i=0;i<size-1;i++){
-	cout<<buffer[i];}
-  	reply = buffer;
-  	return reply;
+	}
+	buffer[size - 1] = '\0';
+	reply = buffer;
+	return reply;
 }
-void TCPClient::receiveFile(int fileSize,int i)
+void TCPClient::receiveFile(int fileSize, int i)
 {
-char b[10];
-sprintf(b,"%d.jpg",i);
-  	const char *file_name = (const char*)&b;
+	char b[10];
+	sprintf(b, "%d.jpg", i);
+	const char *file_name = (const char *)&b;
 
 	// 打开文件，准备写入
 	FILE *fp = fopen(file_name, "w");
@@ -100,34 +97,33 @@ sprintf(b,"%d.jpg",i);
 	char buffer[BUFFER_SIZE];
 	bzero(buffer, BUFFER_SIZE);
 	int length = 0;
-	cout<<"receive file----"<<endl;
-	int rest=fileSize;
+	int rest = fileSize;
 	while ((rest) > 0)
 	{
-	length = recv(sock, buffer, BUFFER_SIZE, 0);
+		length = recv(sock, buffer, BUFFER_SIZE, 0);
 		if (fwrite(buffer, sizeof(char), length, fp) < length)
 		{
 			printf("File:\t%s Write Failed\n", file_name);
 			break;
 		}
 		bzero(buffer, BUFFER_SIZE);
-		rest-=length;
+		rest -= length;
 	}
 	// 接收成功后，关闭文件，关闭socket
 	printf("Receive File:\t%s From Server IP Successful!\n", file_name);
 }
 
-
 string TCPClient::read()
 {
-  	char buffer[1] = {};
-  	string reply;
-  	while (buffer[0] != '\n') {
-    		if( recv(sock , buffer , sizeof(buffer) , 0) < 0)
-    		{
-      			cout << "receive failed!" << endl;
+	char buffer[1] = {};
+	string reply;
+	while (buffer[0] != '\n')
+	{
+		if (recv(sock, buffer, sizeof(buffer), 0) < 0)
+		{
+			cout << "receive failed!" << endl;
 			return nullptr;
-    		}
+		}
 		reply += buffer[0];
 	}
 	return reply;
@@ -135,5 +131,5 @@ string TCPClient::read()
 
 void TCPClient::exit()
 {
-    close( sock );
+	close(sock);
 }
